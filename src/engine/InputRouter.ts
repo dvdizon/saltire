4ec18011371bf71982d554af94cad213841ac487
David Dizon <daviddizon@gmail.com>
2026-01-31 22:30:39 -0800

    feat: add drag panning for the camera
    Move tile/entity selection to pointer-up and pan the camera when dragging.
    
    Testing: not run (not requested)

--- src/engine/InputRouter.ts
+++ src/engine/InputRouter.ts
@@ -5,6 +5,13 @@ import { screenToGrid, TILE_H } from './IsoRenderer'
 export class InputRouter implements IInputRouter {
   private tileSelectedCallbacks: TileSelectedCallback[] = []
   private entityTappedCallbacks: EntityTappedCallback[] = []
+  private dragState: {
+    startX: number
+    startY: number
+    lastX: number
+    lastY: number
+    active: boolean
+  } | null = null
 
   constructor(
     private scene: Phaser.Scene,
@@ -13,6 +20,7 @@ export class InputRouter implements IInputRouter {
   ) {
     this.scene.input.on('pointerdown', this.handlePointerDown)
     this.scene.input.on('pointermove', this.handlePointerMove)
+    this.scene.input.on('pointerup', this.handlePointerUp)
   }
 
   onTileSelected(callback: TileSelectedCallback): void {
@@ -24,6 +32,32 @@ export class InputRouter implements IInputRouter {
   }
 
   private handlePointerDown = (pointer: Phaser.Input.Pointer): void => {
+    this.dragState = {
+      startX: pointer.x,
+      startY: pointer.y,
+      lastX: pointer.x,
+      lastY: pointer.y,
+      active: false,
+    }
+  }
+
+  private handlePointerMove = (pointer: Phaser.Input.Pointer): void => {
+    if (this.handleDrag(pointer)) {
+      return
+    }
+
+    this.updateHoverCursor(pointer)
+  }
+
+  private handlePointerUp = (pointer: Phaser.Input.Pointer): void => {
+    const wasDragging = this.dragState?.active ?? false
+    this.dragState = null
+
+    if (wasDragging) {
+      this.updateHoverCursor(pointer)
+      return
+    }
+
     const { width, height } = this.scene.scale
     const { originX, originY } = this.getOrigin(width, height)
     const { row, col } = screenToGrid(pointer.worldX, pointer.worldY, originX, originY)
@@ -44,7 +78,35 @@ export class InputRouter implements IInputRouter {
     this.tileSelectedCallbacks.forEach((callback) => callback(row, col))
   }
 
-  private handlePointerMove = (pointer: Phaser.Input.Pointer): void => {
+  private handleDrag(pointer: Phaser.Input.Pointer): boolean {
+    if (!this.dragState || !pointer.isDown) {
+      return false
+    }
+
+    const deltaX = pointer.x - this.dragState.startX
+    const deltaY = pointer.y - this.dragState.startY
+    const distance = Math.hypot(deltaX, deltaY)
+
+    if (!this.dragState.active && distance > 6) {
+      this.dragState.active = true
+      this.scene.input.setDefaultCursor('grabbing')
+    }
+
+    if (!this.dragState.active) {
+      return false
+    }
+
+    const moveX = pointer.x - this.dragState.lastX
+    const moveY = pointer.y - this.dragState.lastY
+    this.scene.cameras.main.scrollX -= moveX
+    this.scene.cameras.main.scrollY -= moveY
+    this.dragState.lastX = pointer.x
+    this.dragState.lastY = pointer.y
+
+    return true
+  }
+
+  private updateHoverCursor(pointer: Phaser.Input.Pointer): void {
     const { width, height } = this.scene.scale
     const { originX, originY } = this.getOrigin(width, height)
     const { row, col } = screenToGrid(pointer.worldX, pointer.worldY, originX, originY)
